c1dabc2f9f4f73901d9e65ea09d5a324373d91e2
M Charity <mastermilkx@gmail.com>
2023-01-06 19:46:17 -0500

    BMO range of faces

--- UI_Prototype/code1.js
+++ UI_Prototype/code1.js
@@ -42,6 +42,21 @@ var bmo_face_canvas = document.getElementById("bmo-face");
 bmo_face_canvas.width = 280;
 bmo_face_canvas.height = 175;
 var bftx = bmo_face_canvas.getContext("2d");
+
+//sprite sheet bmo face
+var bmo_face_ss = new Image();
+bmo_face_ss.src = "../data/imgs/bmo_sprite_sheet.png";
+let face_width = 800;
+let face_height = 606;
+let faces_per_row = 9;
+let expressions = [
+    ["content","huh", "neutral", "sus", "D:", "skeptical", "oh", "sound", ":)"],
+    ["-_-", ">:(", "look", "hmm", "confused", "scared", ":(", "OH", "ugh"],
+    ["5min", "u", "happy", ":D", "smile", "challenge", "annoyed", "transparent", "noface"]
+]
+
+
+//default bmo face
 var bmo_face = new Image();
 bmo_face.src = "../data/imgs/bmo_blank.png";
 
@@ -97,14 +112,33 @@ function toggleChat(){
 
 // - face functions - //
 
-//set default bmo face
-bmo_face.onload = function(){
+//return the row and column index of the face in the sprite sheet
+function getFace(mood){
+    for (let i = 0; i < expressions.length; i++){
+        for (let j = 0; j < expressions[i].length; j++){
+            if (expressions[i][j] == mood){
+                return [i,j]
+            }
+        }
+    }
+    return [-1, -1] //if the mood is not found
+}
+
+//draw the face of BMO based on the mood given
+function drawBMOFace(mood){
     //add background
     bftx.fillStyle = "#4CAA98";
     bftx.fillRect(0, 0, 280, 175);
 
     //draw the face
-    bftx.drawImage(bmo_face, 0, 0, bmo_face.width, bmo_face.height, 0, 0, 280, 175);
+    let face = getFace(mood);
+    if(face == [-1, -1]){
+        console.log(`Mood ${mood} not found!`)
+        face = [8,8]; //if the mood is not found, draw the nothing face
+    }
+    let x = face[1]*face_width;
+    let y = face[0]*face_height;
+    bftx.drawImage(bmo_face_ss, x, y, face_width, face_height, 0, 0, 280, 175);
 
     //draw a border around
     bftx.strokeStyle = "#4CAA98";
@@ -121,6 +155,30 @@ bmo_face.onload = function(){
 
 }
 
+// //set default bmo face
+// bmo_face.onload = function(){
+//     //add background
+//     bftx.fillStyle = "#4CAA98";
+//     bftx.fillRect(0, 0, 280, 175);
+
+//     //draw the face
+//     bftx.drawImage(bmo_face, 0, 0, bmo_face.width, bmo_face.height, 0, 0, 280, 175);
+
+//     //draw a border around
+//     bftx.strokeStyle = "#4CAA98";
+//     bftx.lineWidth = 30;
+//     bftx.beginPath();
+//     bftx.roundRect(0, 0, 280, 175,[40]);
+//     bftx.stroke();
+
+//     bftx.strokeStyle = "#000"
+//     bftx.lineWidth = 2;
+//     bftx.beginPath();
+//     bftx.roundRect(14, 13, 250, 146, 25);
+//     bftx.stroke();
+
+// }
+
 // - chat functions - //
 
 //basic chatbox function
@@ -133,6 +191,14 @@ chatbox.addEventListener("keyup", function(event){
         chatbox.value = "";
         chat_out.innerHTML += chatbox_text + "<br>";
         chat_out.scrollTop = chat_out.scrollHeight;
+
+        /// DEBUGGING IN THE CHAT ///
+        //change bmo's face to the input if a keyword is found
+        if(chatbox_text.includes("mood: ")){
+            let new_mood = chatbox_text.replace("mood: ", "");
+            console.log(`new mood: ${new_mood}`)
+            drawBMOFace(new_mood);
+        }
     }
 });
 
@@ -167,6 +233,9 @@ function init(){
     //set the default icon
     let bmo_shown = document.getElementById("right-half");
     showIcon((bmo_shown.style.display == "block" ? "off" : "awake"));
+
+    //set bmo's face
+    drawBMOFace(":)");
 
     //set some sample code
     ace_editor.setValue("//BMO CODE \
