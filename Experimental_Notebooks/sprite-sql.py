7bcd807946e96c170841f5c45b6e854603db830c
dipikarajesh18 <dipika.rajesh@gmail.com>
2023-04-17 18:21:10 -0400

    update sleep

--- Experimental_Notebooks/sprite-sql.py
+++ Experimental_Notebooks/sprite-sql.py
@@ -4,6 +4,7 @@ import random
 import numpy as np
 import os
 import math
+import time
 
 
 #CONNECT TO THE SQL DATABASE
@@ -18,25 +19,35 @@ mycursor = mydb.cursor()
 
 def convert_sprite_to_hex(filename):
     pico_sheets = np.load(filename, allow_pickle=True)
-    count = 0
-    
-    for image_data in pico_sheets:
-        if count == 10:
-            break
+    CUR_SS_COUNT = 1386
+
+    while CUR_SS_COUNT < len(pico_sheets):
+        print(f"CURRENT SPRITE INDEX: {CUR_SS_COUNT}")
+        image_data = pico_sheets[CUR_SS_COUNT]
+        if CUR_SS_COUNT % 3000 == 0 and CUR_SS_COUNT != 0:
+            print("sleeping")
+            time.sleep(3600)
+            print("done sleeping")
         image_dict = {}
         hex_list = np.vectorize(hex)(image_data['img'])
         image_dict['hex_string'] = "".join([hex.split('x')[-1] for l in hex_list for hex in l]).upper()
-        image_dict['pico_sheet_name'] = image_data['pico_sheet_name']
+        image_dict['pico_sheet_name'] = image_data['pico_sheet_name'].split('.spritesheet.png')[0]
         image_dict['sprite_id'] = image_data['sprite_id']
         sql = f"INSERT INTO sprite_labels (PICO8_GAME_ID, SPRITE_INDEX, SPRITE_HEX) VALUES ('{image_dict['pico_sheet_name']}', {image_dict['sprite_id']}, '{image_dict['hex_string']}')"
         mycursor.execute(sql)
-        count += 1
+        mydb.commit()
+        CUR_SS_COUNT+=1
+        
 
 if __name__ == '__main__':
     filename = '../data/rip_data/good_pico_sheets.npy'
-    # convert_sprite_to_hex(filename)
+    convert_sprite_to_hex(filename)
     
-    s = "SELECT * FROM sprite_labels"
-    mycursor.execute(s)
-    sqlRes = mycursor.fetchall()
-    print(sqlRes)
+    # print("display database")
+    # s = "SELECT * FROM sprite_labels LIMIT 10"
+    # s = "DELETE FROM sprite_labels"
+
+    # mycursor.execute(s)
+    # mydb.commit()
+    # sqlRes = mycursor.fetchall()
+    # print(sqlRes)
