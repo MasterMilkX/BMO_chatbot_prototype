8516c40dfc974b819a937f3665c6bdb60dc9a454
dipikarajesh18 <dipika.rajesh@gmail.com>
2023-04-18 07:24:49 -0400

    update sprite-sql code to refresh db creds

--- Experimental_Notebooks/sprite-sql.py
+++ Experimental_Notebooks/sprite-sql.py
@@ -8,26 +8,30 @@ import time
 
 
 #CONNECT TO THE SQL DATABASE
-mydb = mysql.connector.connect(
-  host="us-cdbr-east-06.cleardb.net",
-  user="b50281fd4181c6",
-  password="12b4a1bb",
-  database="heroku_2f2d25ae5fc707a"
-)
+def connect_to_db():
+    mydb = mysql.connector.connect(
+    host="us-cdbr-east-06.cleardb.net",
+    user="b50281fd4181c6",
+    password="12b4a1bb",
+    database="heroku_2f2d25ae5fc707a"
+    )
 
-mycursor = mydb.cursor()
+    mycursor = mydb.cursor()
+    return mydb, mycursor
 
 def convert_sprite_to_hex(filename):
     pico_sheets = np.load(filename, allow_pickle=True)
-    CUR_SS_COUNT = 1386
+    mydb, mycursor = connect_to_db()
+    CUR_SS_COUNT = 4794
 
     while CUR_SS_COUNT < len(pico_sheets):
         print(f"CURRENT SPRITE INDEX: {CUR_SS_COUNT}")
         image_data = pico_sheets[CUR_SS_COUNT]
-        if CUR_SS_COUNT % 3000 == 0 and CUR_SS_COUNT != 0:
+        if CUR_SS_COUNT % 1500 == 0 and CUR_SS_COUNT != 0:
             print("sleeping")
             time.sleep(3600)
             print("done sleeping")
+            mydb, mycursor = connect_to_db
         image_dict = {}
         hex_list = np.vectorize(hex)(image_data['img'])
         image_dict['hex_string'] = "".join([hex.split('x')[-1] for l in hex_list for hex in l]).upper()
@@ -37,17 +41,42 @@ def convert_sprite_to_hex(filename):
         mycursor.execute(sql)
         mydb.commit()
         CUR_SS_COUNT+=1
-        
+
+# render image from hex string
+def render_hex(hex_string):
+    hex_list = [hex_string[i:i+6] for i in range(0, len(hex_string), 6)]
+    rgb_list = [tuple(int(hex_list[i][j:j+2], 16) for j in (0, 2, 4)) for i in range(len(hex_list))]
+    img = Image.new('RGB', (8,8))
+    pixels = img.load()
+    for i in range(len(rgb_list)):
+        x = i % 8
+        y = math.floor(i / 8)
+        pixels[x,y] = rgb_list[i]
+    return img
 
 if __name__ == '__main__':
     filename = '../data/rip_data/good_pico_sheets.npy'
     convert_sprite_to_hex(filename)
     
+    # mydb, mycursor = connect_to_db()
+    # s = "DELETE FROM sprite_labels"
+    # mydb.commit()
+
     # print("display database")
     # s = "SELECT * FROM sprite_labels LIMIT 10"
-    # s = "DELETE FROM sprite_labels"
+    # s = "SELECT user_label, SPRITE_HEX FROM sprite_labels WHERE is_unknown = 1 OR user_label is NOT NULL"
 
     # mycursor.execute(s)
-    # mydb.commit()
-    # sqlRes = mycursor.fetchall()
-    # print(sqlRes)
+    # labelled_sprites = mycursor.fetchall()
+    # print(labelled_sprites)
+    
+    # print(len(labelled_sprites))
+    
+    # for i in range(len(labelled_sprites)):
+    #     print(labelled_sprites[i][0])
+    #     render_hex(labelled_sprites[i][1]).show()
+    
+    
+    
+    
+    
--- Python/utils.py
+++ Python/utils.py
@@ -8,131 +8,133 @@ from nltk.corpus import stopwords
 import numpy as np
 import colorsys
 from PIL import Image
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
 
 
-################################                        GLOBAL VARIABLES                     ################################
+# ################################                        GLOBAL VARIABLES                     ################################
 
-CUSTOM_STOPWORDS = ["game", ",", ".", "!"]
-nlp = spacy.load("en_core_web_sm")
+# CUSTOM_STOPWORDS = ["game", ",", ".", "!"]
+# nlp = spacy.load("en_core_web_sm")
 
 # COLOR FUNCTIONS
 PICO_PALETTE = ['#000000','#1D2B53','#7E2553','#008751','#AB5236','#5F574F','#C2C3C7','#FFF1E8','#FF004D','#FFA300','#FFEC27','#00E436','#29ADFF','#83769C','#FF77A8','#FFCCAA']
 
 
 
-################################                        TF-IDF FUNCTIONS                      ################################
+# ################################                        TF-IDF FUNCTIONS                      ################################
 
-# used in the context where docs = games and their descriptional entities
+# # used in the context where docs = games and their descriptional entities
 
-#term frequency - how often a term appears in a document / total number of terms in the document
-def tf(doc):  # assume doc is a list of words already tokenized
-    tf_dict = {}
-    for word in doc:
-        if word not in tf_dict:
-            tf_dict[word] = 1
-        tf_dict[word] += 1
-    for word in tf_dict:
-        tf_dict[word] = tf_dict[word] / len(doc)
-    return tf_dict
+# #term frequency - how often a term appears in a document / total number of terms in the document
+# def tf(doc):  # assume doc is a list of words already tokenized
+#     tf_dict = {}
+#     for word in doc:
+#         if word not in tf_dict:
+#             tf_dict[word] = 1
+#         tf_dict[word] += 1
+#     for word in tf_dict:
+#         tf_dict[word] = tf_dict[word] / len(doc)
+#     return tf_dict
 
-#inverse document frequency - log(total number of documents / number of documents with term t in it)
-def idf(documents):  # assume documents is a list of lists of words already tokenized
-    df = {}
-    for doc in documents:  
-        for word in doc:
-            if word not in df:
-                df[word] = 0
-            df[word] += 1
-    idf_dict = {}
-    for word in df:
-        idf_dict[word] = math.log(len(documents) / df[word])
-    return idf_dict
+# #inverse document frequency - log(total number of documents / number of documents with term t in it)
+# def idf(documents):  # assume documents is a list of lists of words already tokenized
+#     df = {}
+#     for doc in documents:  
+#         for word in doc:
+#             if word not in df:
+#                 df[word] = 0
+#             df[word] += 1
+#     idf_dict = {}
+#     for word in df:
+#         idf_dict[word] = math.log(len(documents) / df[word])
+#     return idf_dict
     
-# get the full tfidf score for each word in each game's dataset
-def tfidf(doc_set):  # assume doc_set is a dictionary of games with word lists already tokenized
-    tfidf_dict = {}
-    corpuses = [d for d in doc_set.values()]
-    idf_dat = idf(corpuses)
-    for game, doc in doc_set.items():
-        tf_dat = tf(doc)
-        tfidf_dict[game] = {}
-        for word in doc:
-            tfidf_dict[game][word] = tf_dat[word] * idf_dat[word]  
-    return tfidf_dict
+# # get the full tfidf score for each word in each game's dataset
+# def tfidf(doc_set):  # assume doc_set is a dictionary of games with word lists already tokenized
+#     tfidf_dict = {}
+#     corpuses = [d for d in doc_set.values()]
+#     idf_dat = idf(corpuses)
+#     for game, doc in doc_set.items():
+#         tf_dat = tf(doc)
+#         tfidf_dict[game] = {}
+#         for word in doc:
+#             tfidf_dict[game][word] = tf_dat[word] * idf_dat[word]  
+#     return tfidf_dict
 
-# same as above, but only uses the idf data from the given doc_set
-def idf_docs(doc_set):
-    idf_dict = {}
-    corpuses = [d for d in doc_set.values()]
-    idf_dat = idf(corpuses)
-    for game, doc in doc_set.items():
-        idf_dict[game] = {}
-        for word in doc:
-            idf_dict[game][word] = idf_dat[word]  
-    return idf_dict
+# # same as above, but only uses the idf data from the given doc_set
+# def idf_docs(doc_set):
+#     idf_dict = {}
+#     corpuses = [d for d in doc_set.values()]
+#     idf_dat = idf(corpuses)
+#     for game, doc in doc_set.items():
+#         idf_dict[game] = {}
+#         for word in doc:
+#             idf_dict[game][word] = idf_dat[word]  
+#     return idf_dict
 
 
 
-################################               IMPORT THE SCRAPED GAME DATA             ################################
+# ################################               IMPORT THE SCRAPED GAME DATA             ################################
 
-#import the game data from the game_datfeat.txt file
-def importGameData(full=True):
-    #select which file to import
-    if full:
-        print("Importing full game data...")
-        game_import_file = "../data/game_datfeat_FULL.txt"
-    else:
-        print("Importing subset of game data...")
-        game_import_file = "../data/game_datfeat.txt"
+# #import the game data from the game_datfeat.txt file
+# def importGameData(full=True):
+#     #select which file to import
+#     if full:
+#         print("Importing full game data...")
+#         game_import_file = "../data/game_datfeat_FULL.txt"
+#     else:
+#         print("Importing subset of game data...")
+#         game_import_file = "../data/game_datfeat.txt"
 
-    #import the data
-    DAT = {}
-    with open(game_import_file, "r") as f:
-        lines = [l.strip() for l in f.readlines()]
-        CUR_GAME = ""
-        for l in lines:
-            # empty line (between entries)
-            if l == "":
-                continue
-            #new entry
-            else:
-                if l[0] == "+":
-                    CUR_GAME = l[2:].upper()
-                    DAT[CUR_GAME] = {"tags":[],"entities":[],"features":[]}
-                elif l[0] == "#":
-                    DAT[CUR_GAME]["tags"] = [t.lower() for t in l[2:].split(",")]
-                elif l[0] == "@":
-                    DAT[CUR_GAME]["entities"] = [e.lower() for e in l[2:].split(",")]
-                elif l[0] == "-":
-                    DAT[CUR_GAME]["features"].append(l[2:])
+#     #import the data
+#     DAT = {}
+#     with open(game_import_file, "r") as f:
+#         lines = [l.strip() for l in f.readlines()]
+#         CUR_GAME = ""
+#         for l in lines:
+#             # empty line (between entries)
+#             if l == "":
+#                 continue
+#             #new entry
+#             else:
+#                 if l[0] == "+":
+#                     CUR_GAME = l[2:].upper()
+#                     DAT[CUR_GAME] = {"tags":[],"entities":[],"features":[]}
+#                 elif l[0] == "#":
+#                     DAT[CUR_GAME]["tags"] = [t.lower() for t in l[2:].split(",")]
+#                 elif l[0] == "@":
+#                     DAT[CUR_GAME]["entities"] = [e.lower() for e in l[2:].split(",")]
+#                 elif l[0] == "-":
+#                     DAT[CUR_GAME]["features"].append(l[2:])
 
-    return DAT
+#     return DAT
 
-#get all of the tags and the entities from the game data
-# returns a tuple of (tags, entities)
-def getTagsEntities(game_data):
-    # get all of the tags and entities
-    ALL_TAGS = []
-    ALL_ENTITIES = []
-    for g in game_data:
-        ALL_TAGS += [t.lower() for t in game_data[g]["tags"]]
-        ALL_ENTITIES += [e.lower() for e in game_data[g]["entities"]]
+# #get all of the tags and the entities from the game data
+# # returns a tuple of (tags, entities)
+# def getTagsEntities(game_data):
+#     # get all of the tags and entities
+#     ALL_TAGS = []
+#     ALL_ENTITIES = []
+#     for g in game_data:
+#         ALL_TAGS += [t.lower() for t in game_data[g]["tags"]]
+#         ALL_ENTITIES += [e.lower() for e in game_data[g]["entities"]]
 
-    # remove duplicates and set the value
-    return list(set(ALL_TAGS)), list(set(ALL_ENTITIES))
+#     # remove duplicates and set the value
+#     return list(set(ALL_TAGS)), list(set(ALL_ENTITIES))
 
-# tokenize a prompt from the list of tags
-def tokenize(txt):
-    raw_toks = word_tokenize(txt)
-    toks = [w.lower() for w in raw_toks if w.lower() not in stopwords.words("english") and w.lower() not in CUSTOM_STOPWORDS]
-    return toks
+# # tokenize a prompt from the list of tags
+# def tokenize(txt):
+#     raw_toks = word_tokenize(txt)
+#     toks = [w.lower() for w in raw_toks if w.lower() not in stopwords.words("english") and w.lower() not in CUSTOM_STOPWORDS]
+#     return toks
 
-# extract and singularize the nouns from a prompt
-def single_noun(txt):
-    doc = nlp(txt)
-    toks = [t.lemma_ for t in doc if t.pos_ == "NOUN"]
-    return toks
+# # extract and singularize the nouns from a prompt
+# def single_noun(txt):
+#     doc = nlp(txt)
+#     toks = [t.lemma_ for t in doc if t.pos_ == "NOUN"]
+#     return toks
 
 
 
@@ -215,3 +217,42 @@ def picoSS2np(ss_file, palette=PICO_PALETTE):
     pico_sheet = readSpriteSheet(ss_file,palette)
     pico_sprites = getSprites(pico_sheet,8,0)
     return np.array(pico_sprites)
+
+# show a single sprite - using a specified palette (form (NxN))
+def showSprPalette(spr,palette=PICO_PALETTE):
+    #set the colormap
+    colormap = ListedColormap(palette,N=len(palette))
+
+    #show the sprite
+    plt.figure(figsize=(2,2))
+    plt.axis('off')
+    plt.imshow(spr.squeeze(),cmap=colormap,vmin=0,vmax=len(palette)-1)
+    plt.show()
+
+
+# show a list of sprites - using a specified palette (form Ax(NxN))
+def showMultiSprPalette(sprs,palette=PICO_PALETTE,text='',textArr=None):
+    #set the colormap
+    colormap = ListedColormap(palette,N=len(palette))
+
+    #show sprites (8 per row)
+    plt.figure(figsize=(20,7))
+    col = 8
+    for i, im in enumerate(sprs):
+        plt.subplot(int(len(sprs) / col) + 1, col, i + 1)
+        plt.imshow(im.squeeze(),cmap=colormap,vmin=0,vmax=len(palette)-1)
+        plt.axis('off')
+
+        #add sublabels
+        if (textArr != None) and (i < len(textArr)):
+            addtext = ""
+            #sub label + title on middle entry
+            if text != '' and i == (min(len(sprs),col)//2):
+                addtext = f"{text.upper()}\n"
+            plt.title(f"{addtext}{textArr[i]}")
+            
+        #add plot title only
+        else:
+            if text != '' and i == (min(len(sprs),col)//2):
+                plt.title(text.upper())
+    plt.show()
